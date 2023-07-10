59590087b2674e71ec52c261edcb6028d16bd70c
Vitaliy Chiley <6439018+vchiley@users.noreply.github.com>
2023-07-10 11:18:37 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,20 @@
 from setuptools import setup, find_packages
 import torch
 
+
+install_requires=[
+    # TODO(tgale): Update this to a stable Triton release once the software pipelining feature is supported.
+    'triton @ git+https://github.com/openai/triton.git@787cdff#subdirectory=python&egg=triton'
+]
+
+extra_deps = {}
+
+extra_deps['dev'] = [
+    'absl-py',
+]
+
+extra_deps['all'] = set(dep for deps in extra_deps.values() for dep in deps)
+
 setup(
     name="stanford-stk",
     version="0.0.5",
@@ -16,11 +30,6 @@ setup(
         "Operating System :: Unix",
     ],
     packages=find_packages(),
-    install_requires=[
-        "absl-py",
-        "numpy",
-        "torch",
-        # TODO(tgale): Update this to a stable Triton release once the software pipelining feature is supported.
-        "triton @ git+https://github.com/openai/triton.git@787cdff#egg=triton&subdirectory=python"
-    ],
+    install_requires=install_requires,
+    extras_require=extra_deps,
 )
